2829eb86c8027846f4d3024261b38ab98422512a
o1egs <tankistonline21@gmail.com>
2022-04-07 20:03:16 +0400

    123

---
+++ index/script.js
@@ -0,0 +1,10 @@
+function HoverOnBtn() {
+    document.body.style.transition = '.5s ease';
+    document.body.style.backgroundImage = 'url(index__img/BackGroundImage_2.png)';
+}
+
+function HoverOffBtn() {
+    document.body.style.transition = '.5s';
+    document.body.style.backgroundImage = 'url(index__img/BackGroundImage_1.png)';
+}
+
